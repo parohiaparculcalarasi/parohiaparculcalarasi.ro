8eb5cc521dc8d79d9b9cf43e5519c5515b77ec7a
Mihai CAZACU <cazacugmihai@gmail.com>
2022-11-20 16:39:19 +0200

    WIP

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# theme: https://github.com/razonyang/hugo-theme-bootstrap/releases/tag/v1.0.0-alpha.15
+# cd themes/hugo-theme-bootstrap
+# git config pull.rebase false
+# git pull
+# rm node_modules
+# npm install
+# npm update
+
+
 clear
 rm -rf ./public
 sudo hugo server -D --port=8033
