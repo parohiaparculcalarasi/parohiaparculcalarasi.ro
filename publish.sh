f10957b474023c2517643257dded041446dc92e4
Mihai CAZACU <cazacugmihai@gmail.com>
2022-11-20 16:13:59 +0200

    WIP

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ssh-add -l
+# ssh-add ~/.ssh/parohiaparculcalarasi.ro/id_ed25519
+
+find ../parohiaparculcalarasi.github.io/ -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*' -exec rm -r {} \;
+find ../parohiaparculcalarasi.github.io/ -mindepth 1 -maxdepth 1 -type f -name '*ml' -exec rm -r {} \;
+
+rm -rf public
+hugo --environment production
+yes | cp -f -r public/. ../parohiaparculcalarasi.github.io/
+cd ../parohiaparculcalarasi.github.io/
+
+git add .
+git commit -m "wip"
+git push origin master
