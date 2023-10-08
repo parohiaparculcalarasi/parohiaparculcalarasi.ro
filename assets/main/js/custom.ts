cc3c8682183f61d4a563b09e159c4169d910e255
Mihai CAZACU <cazacugmihai@gmail.com>
2023-10-08 21:28:46 +0300

    popover

---
+++ assets/main/js/custom.ts
@@ -0,0 +1,11 @@
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+
+// https://getbootstrap.com/docs/5.2/components/popovers/
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
+    // html: true
+    // container: 'body'
+}));
+
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
